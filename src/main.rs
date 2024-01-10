4f1201780ed878a62d7c1f052b479d2d714dfa93
spineda2019 <spineda@wpi.edu>
2024-01-10 13:17:15 -0500

    setup todo list

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ mod symbol_table;
 use symbol_table::SymbolTable;
 
 fn main() {
+    println!("Hello, Compiler!");
+    // Open File
+    // Parse whitespace delimited words
+    // Classify as tokens
+    // load in to table
     let symbol_table = SymbolTable::init();
-    println!("Hello, world!");
 }
