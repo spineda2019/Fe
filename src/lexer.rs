b550c0628d6af86ddd2396d5df0aa0033a0d47b2
spineda2019 <spineda@wpi.edu>
2024-01-10 15:39:57 -0500

    lexer now classifies words

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,32 @@
 use crate::symbol_table::token::Token;
-use std::fs::File;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+const VALID_OPERATORS: [&str; 4] = ["*", "+", "/", "-"];
+
+fn classify_word(word: &str) -> Token {
+    match word {
+        x if VALID_OPERATORS.contains(&x) => Token::new_operator(x),
+        y if y.parse::<isize>().is_ok() => Token::new_number_literal(y),
+        z => Token::new_identifier(z),
+    }
+}
 
 pub fn tokenize_file(file: &File) -> Result<Vec<Token>, std::io::Error> {
+    let reader: BufReader<&File> = BufReader::new(file);
+    let mut line: String = String::new();
+
+    for file_line in reader.lines() {
+        line = match file_line {
+            Ok(x) => x,
+            Err(y) => return Err(y),
+        };
+
+        for word in line.split_whitespace() {
+            classify_word(word);
+        }
+    }
     todo!();
 }
