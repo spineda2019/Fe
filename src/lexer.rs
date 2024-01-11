07a68fbd89f6c94cbb164b61bc0f06373fc38da7
spineda2019 <spineda@wpi.edu>
2024-01-11 08:24:37 -0500

    cleaner parsing AND added some stypes and puncs

--- src/lexer.rs
+++ src/lexer.rs
@@ -4,7 +4,24 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-const VALID_OPERATORS: [char; 7] = ['*', '+', '/', '-', '=', ':', ';'];
+const VALID_OPERATORS: [char; 6] = ['*', '+', '/', '-', '=', ':'];
+const VALID_GROUPING_SYMBOLS: [char; 4] = ['{', '}', '(', ')'];
+const VALID_PUNCTUATIONS: [char; 1] = [';'];
+const VALID_TYPE_NAMES: [&str; 9] = [
+    "sint8", "uint8", "sint16", "uint16", "sint32", "uint32", "sint64", "uint64", "size",
+];
+
+#[inline]
+fn is_a_fe_type(word: &str) -> bool {
+    VALID_TYPE_NAMES.contains(&word)
+}
+
+#[inline]
+fn separates_a_lexeme(character: &char) -> bool {
+    VALID_OPERATORS.contains(character)
+        || character.is_whitespace()
+        || VALID_PUNCTUATIONS.contains(character)
+}
 
 #[inline]
 fn is_an_operator(word: &str) -> bool {
@@ -16,9 +33,21 @@ fn is_an_operator(word: &str) -> bool {
     VALID_OPERATORS.contains(&charred_word)
 }
 
+#[inline]
+fn is_a_punctuation(word: &str) -> bool {
+    let charred_word: char = match word.parse::<char>() {
+        Ok(x) => x,
+        Err(_) => return false,
+    };
+
+    VALID_PUNCTUATIONS.contains(&charred_word)
+}
+
 fn classify_word(word: &str) -> Token {
     match word {
-        x if is_an_operator(x) => Token::new_operator(x),
+        op if is_an_operator(op) => Token::new_operator(op),
+        punc if is_a_punctuation(punc) => Token::new_punctuation(punc),
+        ty if is_a_fe_type(ty) => Token::new_type_name(ty),
         y if y.parse::<isize>().is_ok() => Token::new_number_literal(y),
         z => Token::new_identifier(z),
     }
@@ -40,27 +69,32 @@ pub fn tokenize_file(file: &File) -> Result<Vec<Token>, std::io::Error> {
             continue;
         }
 
-        let mut most_recent_token_string: String = String::new();
+        let mut most_recent_lexeme: String = String::new();
         for character in line.chars() {
-            if !VALID_OPERATORS.contains(&character) && !character.is_whitespace() {
-                most_recent_token_string.push(character);
+            if !separates_a_lexeme(&character) {
+                most_recent_lexeme.push(character);
                 continue;
             }
-            if most_recent_token_string == "//" {
+
+            if most_recent_lexeme == "//" {
                 // Immeditately going to next line if we see a comment saves
                 // time and memory!
                 break;
             }
 
-            if !most_recent_token_string.is_empty() {
-                tokens.push(classify_word(&most_recent_token_string));
+            dbg!(&most_recent_lexeme);
+            dbg!(&character);
+
+            if !most_recent_lexeme.is_empty() {
+                tokens.push(classify_word(&most_recent_lexeme));
             }
             if !character.is_whitespace() {
                 tokens.push(classify_word(&character.to_string()));
             }
 
-            most_recent_token_string.clear();
+            most_recent_lexeme.clear();
         }
+        // TODO: check if lexeme has not been cleared. If so, bad syntax
     }
 
     Ok(tokens)
