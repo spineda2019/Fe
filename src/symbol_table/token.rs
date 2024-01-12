2de0484cab85ac7569c4629797c51ada674890d8
spineda2019 <spineda@wpi.edu>
2024-01-12 18:10:58 -0500

    ctors now return results for error propagation

--- src/symbol_table/token.rs
+++ src/symbol_table/token.rs
@@ -1,3 +1,5 @@
+use std::io::Error;
+
 #[derive(Debug)]
 pub enum Token {
     NumberLiteral(isize),
@@ -12,61 +14,69 @@ pub enum Token {
 }
 
 impl Token {
-    pub fn new_number_literal(number_literal: &str) -> Self {
-        let number: isize = match number_literal.parse::<isize>() {
-            Ok(x) => x,
-            Err(_) => panic!("This should not happen! {number_literal}"),
-        };
-        Token::NumberLiteral(number)
+    pub fn new_number_literal(number_literal: &str) -> Result<Self, std::io::Error> {
+        match number_literal.parse::<isize>() {
+            Ok(x) => Ok(Token::NumberLiteral(x)),
+            Err(_) => {
+                let error_message: String = format!("{number_literal}: Not parseable to isize");
+                Err(Error::new(std::io::ErrorKind::Interrupted, error_message))
+            }
+        }
     }
 
-    pub fn new_identifier(identifier: &str) -> Self {
-        Token::Identifier(identifier.to_string())
+    pub fn new_identifier(identifier: &str) -> Result<Self, std::io::Error> {
+        Ok(Token::Identifier(identifier.to_string()))
     }
 
-    pub fn new_operator(operator: &str) -> Self {
-        let operator_char: char = match operator.parse::<char>() {
-            Ok(x) => x,
-            Err(_) => panic!("This should not happen! {operator}"),
-        };
-        Token::Operator(operator_char)
+    pub fn new_operator(operator: &str) -> Result<Self, std::io::Error> {
+        match operator.parse::<char>() {
+            Ok(x) => Ok(Token::Operator(x)),
+            Err(_) => {
+                let error_message: String = format!("{operator}: Not parseable to char");
+                Err(Error::new(std::io::ErrorKind::Interrupted, error_message))
+            }
+        }
     }
 
-    pub fn new_punctuation(punctuation: &str) -> Self {
-        let punctuation_char: char = match punctuation.parse::<char>() {
-            Ok(x) => x,
-            Err(_) => panic!("This should not happen! {punctuation}"),
-        };
-        Token::Punctuation(punctuation_char)
+    pub fn new_punctuation(punctuation: &str) -> Result<Self, std::io::Error> {
+        match punctuation.parse::<char>() {
+            Ok(x) => Ok(Token::Punctuation(x)),
+            Err(_) => {
+                let error_message: String = format!("{punctuation}: Not parseable to char");
+                Err(Error::new(std::io::ErrorKind::Interrupted, error_message))
+            }
+        }
     }
 
-    pub fn new_type_name(type_name: &str) -> Self {
-        Token::TypeName(type_name.to_string())
+    pub fn new_type_name(type_name: &str) -> Result<Self, std::io::Error> {
+        Ok(Token::TypeName(type_name.to_string()))
     }
 
-    pub fn new_grouping_symbol(grouping_symbol: &str) -> Self {
-        let group_char: char = match grouping_symbol.parse::<char>() {
-            Ok(x) => x,
-            Err(_) => panic!("This should not happen! {grouping_symbol}"),
-        };
-
-        Token::GroupingSymbol(group_char)
+    pub fn new_grouping_symbol(grouping_symbol: &str) -> Result<Self, std::io::Error> {
+        match grouping_symbol.parse::<char>() {
+            Ok(x) => Ok(Token::GroupingSymbol(x)),
+            Err(_) => {
+                let error_message: String = format!("{grouping_symbol}: Not parseable to char");
+                Err(Error::new(std::io::ErrorKind::Interrupted, error_message))
+            }
+        }
     }
 
-    pub fn new_class_region(class_region: &str) -> Self {
-        Token::ClassRegion(class_region.to_string())
+    pub fn new_class_region(class_region: &str) -> Result<Self, std::io::Error> {
+        Ok(Token::ClassRegion(class_region.to_string()))
     }
 
-    pub fn new_declartion_keyword(declaration_keyword: &str) -> Self {
-        Token::DeclarationKeyword(declaration_keyword.to_string())
+    pub fn new_declartion_keyword(declaration_keyword: &str) -> Result<Self, std::io::Error> {
+        Ok(Token::DeclarationKeyword(declaration_keyword.to_string()))
     }
 
-    pub fn new_scope_edge(scope_edge: &str) -> Self {
-        let scope_edge_char: char = match scope_edge.parse::<char>() {
-            Ok(x) => x,
-            Err(_) => panic!("This should not happen! {scope_edge}"),
-        };
-
-        Token::ScopeEdge(scope_edge_char)
+    pub fn new_scope_edge(scope_edge: &str) -> Result<Self, std::io::Error> {
+        match scope_edge.parse::<char>() {
+            Ok(x) => Ok(Token::ScopeEdge(x)),
+            Err(_) => {
+                let error_message: String = format!("{scope_edge}: Not parseable to char");
+                Err(Error::new(std::io::ErrorKind::Interrupted, error_message))
+            }
+        }
     }
 }
